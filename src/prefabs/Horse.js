e75df0936e5a3019ed1320fab34257782b6f6593
Aria Altenburg <aria.altenburg@gmail.com>
2021-04-25 23:32:01 -0700

    added horse entity to play scene

--- src/prefabs/Horse.js
+++ src/prefabs/Horse.js
@@ -3,7 +3,7 @@ class Horse extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to exisiting scene
-        this.moveSpeed = game.settings.horseSpeed;         // pixels per frame
+        //this.moveSpeed = game.settings.horseSpeed;         // pixels per frame
     }
 
     update() {
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,11 +7,18 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('background', './assets/playBackground.png');
+        this.load.image('horse', './assets/horse.png');
     }
 
     create() {
         // background
         this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0);
+
+        // UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xf3f8e2).setOrigin(0,0);
+
+        // add player horse
+        this.p1Horse = new Horse(this, 50, 300, 'horse', 128, 80).setOrigin(0, 0);
         
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize/*height*/, '0xFFFFFF').setOrigin(0, 0);    // top
