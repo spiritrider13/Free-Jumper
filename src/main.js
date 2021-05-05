45da1814b8d2310ad0ba4fd8d5625f6caef58f26
samhvik <samhvik@gmail.com>
2021-05-05 13:13:55 -0700

    Added main.js header for grading

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+/*
+
+
+Code written by Sam Vik, Aria Altenburg and Jason Lee.
+
+Something we thought we did technically interesting was our obstacle spawn and hit detection.
+There are 3 total obstacles always-present, however they are called upon each other when reaching
+the end of the screen. Took inspiration from Nathan's "recursive" spawning, but wanted to keep it self-
+contained for performance. Also, while the horse jumps, it actually just ignores the obstacles' hitboxes.
+
+Additionally, our speedModifier variable is technically interesting, at least to Sam Vik. We used it to modify
+the speed of all things in the game, and provided an easy way to balance/adjust the difficulty during playtesting.
+
+In terms of our visual style, we are specifically proud of Jason's work on the background music. It sounds good, real,
+and not cheap. Jason also made a very nice background image for the main menu that we really like.
+We are also very proud of our hand-crafted horse animations made through Libresprite. A horse is a complex animal and
+we think our animations compliment that fact.
+
+
+*/
+
+
+
 // Project info included in README.md
 
 let config = {
