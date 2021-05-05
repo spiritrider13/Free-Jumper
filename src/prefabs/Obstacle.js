ed961fe21a0d06ecf68b2ae408537e784e1cb4f2
samhvik <samhvik@gmail.com>
2021-05-05 13:40:44 -0700

    Cropped big jump and bug fixes

--- src/prefabs/Obstacle.js
+++ src/prefabs/Obstacle.js
@@ -6,20 +6,6 @@ class Obstacle extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);   
 
         this.type = type;
-
-        if(type == 0){ //mud puddle
-            this.texture = 'defaultTexture';
-        }
-        else if(type == 1){
-            this.texture = 'defaultTexture';
-        }
-        else if(type == 2){
-            this.texture = 'defaultTexture';
-        }
-        else{
-            console.error("Invalid obstacle type attempted instatiation");
-        }
-        
         
     }
 
@@ -34,7 +20,7 @@ class Obstacle extends Phaser.GameObjects.Sprite {
         var random = Math.floor(Math.random() * 300); //creates either 0, 1, or 3
 
         this.x = 640 + random;
-        this.y = 360;
+        this.y = 370;
         this.active = true;
         console.log("Obstacle " + this.type + " movement begun");
     }
